7b2bd1369adf346fd848664f828e96451cab117e
Mahesh Peiris <mahesh_sanjaya@yahoo.com>
2024-08-12 19:34:12 -0400

    Update mahesh-branch.py

--- mahesh-branch.py
+++ mahesh-branch.py
@@ -9,3 +9,11 @@ def factorial(m):
         return 1
     else:
         return n * factorial(n-1)
+
+
+
+def factorial(45m):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
